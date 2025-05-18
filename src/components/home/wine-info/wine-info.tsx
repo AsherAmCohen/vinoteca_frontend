ce39e8c41ae49f808948eb401f6590a4e6596f32
AsherAmCohen <154266987+AsherAmCohen@users.noreply.github.com>
2025-05-18 13:58:14 -0600

    Corrigiendo request

--- src/components/home/wine-info/wine-info.tsx
+++ src/components/home/wine-info/wine-info.tsx
@@ -1,6 +1,5 @@
 import {
     Box,
-    Button,
     DialogActions,
     DialogContent,
     Table,
@@ -8,7 +7,9 @@ import {
     TableCell,
     TableContainer,
     TableRow,
-    Paper
+    Paper,
+    Chip,
+    IconButton
 } from "@mui/material";
 
 import { TagSimple as LabelOutlined } from "@phosphor-icons/react";
@@ -17,13 +18,48 @@ import { Tag as Category } from "@phosphor-icons/react"
 import { Article as Description } from "@phosphor-icons/react";
 import { CurrencyDollar as AttachMoney } from "@phosphor-icons/react";
 import { StackOverflowLogo as Inventory } from "@phosphor-icons/react";
-import { Info as Info } from "@phosphor-icons/react";
 import { ShoppingCart as Repeat } from "@phosphor-icons/react";
+import { useSelector } from "react-redux";
+import { useAddShoppingCartMutation, useAmountProductQuery, useUpdateAmountProductMutation } from "../../../store/api/api";
+import { Plus as Add } from "@phosphor-icons/react";
+import { Minus as Remove } from "@phosphor-icons/react";
 
 export const WineInfo = (props: any) => {
     const { category, description, id, image, mark, name, price, sale, stock } = props.args.wine;
 
+    // Obtener imagen
     const apiUrl = `${import.meta.env.VITE_BACKEND_URL}/wine/image?image=`;
+
+    // Obtener el ID del carrito
+    const { shoppingCart } = useSelector((state: any) => state.Auth.user)
+
+    // Obtener cantidad del producto si ya esta agregado al carrito
+    const dataWine = {
+        wineId: id,
+        shoppingCartId: shoppingCart,
+        amount: 0
+    }
+
+    const { data } = useAmountProductQuery(dataWine)
+    const amount = data ? data.data : 0
+
+    const [addWine] = useAddShoppingCartMutation();
+    const [updateAmount] = useUpdateAmountProductMutation();
+
+    const handleAddShoppingCart = () => {
+        addWine(dataWine)
+    }
+
+    const handleAddProduct = () => {
+        dataWine.amount = amount + 1
+        updateAmount(dataWine)
+    }
+
+    const handleDeleteProdudct = () => {
+        dataWine.amount = amount - 1
+        updateAmount(dataWine)
+    }
+
 
     return (
         <>
@@ -112,15 +148,6 @@ export const WineInfo = (props: any) => {
                                     </TableCell>
                                     <TableCell>{stock}</TableCell>
                                 </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                                            <Info fontSize="small" />
-                                            <strong>ID</strong>
-                                        </Box>
-                                    </TableCell>
-                                    <TableCell>{id}</TableCell>
-                                </TableRow>
                             </TableBody>
                         </Table>
                     </TableContainer>
@@ -128,9 +155,65 @@ export const WineInfo = (props: any) => {
             </DialogContent>
 
             <DialogActions>
-                <Button variant="contained" onClick={() => console.log("Botón de acción")}>
-                    Acción
-                </Button>
+                {amount < 1 ?
+                    <Chip
+                        variant="outlined"
+                        clickable={false}
+                        label={
+                            <>
+                                Agregar a carrito
+                                <Repeat/>
+                            </>
+                        }
+                        onClick={handleAddShoppingCart}
+                        sx={{
+                            cursor: 'pointer',
+                            px: 1.5,
+                            py: 0.5,
+                            '.MuiChip-label': { display: 'flex', alignItems: 'center', gap: 1 },
+                            background: 'var(--Vinoteca-Background-Dark)',
+                            color: 'var(--Vinoteca-Background-Light)',
+                        }}
+                    />
+                    :
+                    <Chip
+                        variant="outlined"
+                        label={
+                            <Box display="flex" alignItems="center" gap={1}>
+                                <IconButton
+                                    size="small"
+                                    onClick={(e) => {
+                                        e.stopPropagation(); // para evitar cerrar el chip si se hace clic
+                                        handleDeleteProdudct();
+                                    }}
+                                    sx={{ p: 0.5, color: 'var(--Vinoteca-Background-Light)' }}
+                                >
+                                    <Remove fontSize="small" />
+                                </IconButton>
+
+                                <span><strong>{amount}</strong></span>
+
+                                <IconButton
+                                    size="small"
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        handleAddProduct();
+                                    }}
+                                    sx={{ p: 0.5, color: 'var(--Vinoteca-Background-Light)' }}
+                                >
+                                    <Add fontSize="small" />
+                                </IconButton>
+                            </Box>
+                        }
+                        sx={{
+                            px: 1.5,
+                            py: 0.5,
+                            '.MuiChip-label': { display: 'flex', alignItems: 'center', gap: 1 },
+                            background: 'var(--Vinoteca-Background-Dark)',
+                            color: 'var(--Vinoteca-Background-Light)'
+                        }}
+                    />
+                }
             </DialogActions>
         </>
     );
